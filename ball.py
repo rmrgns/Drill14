a171549807920c4d65def8afd787878215c8b09f
Jung Geuk Hun <111038432+rmrgns@users.noreply.github.com>
2023-11-23 11:05:41 +0900

    Drill14 (2017180039정극훈)

---
+++ ball.py
@@ -0,0 +1,35 @@
+from pico2d import *
+import game_world
+import game_framework
+import random
+
+import server
+
+
+class Ball:
+    image = None
+
+    def __init__(self, x = None, y = None):
+        if Ball.image == None:
+            Ball.image = load_image('ball21x21.png')
+        self.x = x if x else random.randint(50, server.background.w)
+        self.y = y if y else random.randint(50, server.background.h)
+
+    def draw(self):
+        sx, sy = self.x - server.background.window_left, self.y - server.background.window_bottom
+        self.image.draw(sx, sy)
+        #draw_rectangle(*self.get_bb())
+
+    def update(self):
+        pass
+
+    def get_bb(self):
+        # sx, sy = self.x - server.background.window_left, self.y - server.background.window_bottom
+        # return sx - 10, sy - 10, sx + 10, sy + 10
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+
+    def handle_collision(self, group, other):
+        match group:
+            case 'boy:ball':
+                game_world.remove_object(self)
+
